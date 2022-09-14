294b8194c7b8a2321370b9018ffe50b0481678f6
yuanyi <wyuanyi1029@gmail.com>
2022-09-14 15:27:48 +0800

    cards

--- Assignment 2/cards.py
+++ Assignment 2/cards.py
@@ -3,76 +3,59 @@ from __future__ import annotations # for type hints of a class in itself
 from enum import Enum
 
 class Rank(Enum):
+	Two = 2 
+	Three = 3
+	Four = 4
+	Five = 5
+	Six = 6
+	Seven = 7
+	Eight = 8
+	Nine = 9 
+	Ten = 10
+	Jack = 11
+	Queen = 12
+	King = 13 
+	Ace = 14
 	def __lt__(self, other: Rank) -> bool:
-		self.Two = 2 
-		self.Three = 3
-		self.Four = 4
-		self.Five = 5
-		self.Six = 6
-		self.Seven = 7
-		self.Eight = 8
-		self.Nine = 9 
-		self.Ten = 10
-		self.Jack = 11
-		self.Queen = 12
-		self.King = 13 
-		self.Ace = 14
-
-		# self.Two = "Two"
-		# self.Three = "Three"
-		# self.Four = "Four"
-		# self.Five = "Five"
-		# self.Six = "Six"
-		# self.Seven = "Seven"
-		# self.Eight = "Eight"
-		# self.Nine = "Nine"
-		# self.Ten = "Ten"
-		# self.Jack = "Jack"
-		# self.Queen = "Queen"
-		# self.King = "King"
-		# self.Ace = "Ace"
-		# raise NotImplementedError
-
+		return self.value < other.value 
 
 class Suit(Enum):
+	Clubs = 1
+	Diamonds = 2
+	Spades = 3
+	Hearts = 4
 	def __lt__(self, other: Suit) -> bool:
-		self.Clubs = 1
-		self.Diamonds = 2
-		self.Spades = 3
-		self.Hearts = 4
-
-		# self.Clubs = "Clubs"
-		# self.Diamonds = "Diamonds"
-		# self.Spades = "Spades"
-		# self.Hearts = "Hearts"
-		# raise NotImplementedError
-
+		return self.value < other.value
 
 class Card:
-
 	def __init__(self, rank: Rank, suit: Suit) -> None:
-		self.rank = Rank
-		self.suit = Suit
-		# raise NotImplementedError
+		self.rank = rank
+		self.suit = suit
 
-	# def __repr__(self) -> str:
-	# 	raise NotImplementedError
+	def __repr__(self) -> str:
+		self.__str__()
 
 	def __str__(self) -> str:
-		print(self.rank + " of " + self.suit)
-		raise NotImplementedError
+		return f"{self.rank.name} of {self.suit.name}"
 
-	# def __eq__(self, other: Card) -> bool:
-	# 	raise NotImplementedError
+	def __eq__(self, other: Card) -> bool:
+		return self.rank.value == other.rank.value and self.suit.value == other.suit.value
 
-	# def __lt__(self, other: Card) -> bool:
-	# 	raise NotImplementedError
+	def __lt__(self, other: Card) -> bool:
+		# return self.suit.value < other.suit.value or self.rank.value < other.rank.value
+		return (self.suit.value, self.rank.value) < (other.suit.value, other.rank.value) 
 
 
 if __name__ == "__main__":
-	card1 = Card(Rank.Two, Suit.Spades)
+	card1 = Card(Rank.Two, Suit.Clubs)
 	card2 = Card(Rank.Ace, Suit.Hearts)
+	print(f"{card1}, {card2}")
+	print(card2 < card1)
+	print(card1 < card2)
 
-	# print(card1)
-	#you can make some local tests here.
-	# pass
+	card1 = Card(Rank.Ace, Suit.Clubs)
+	card2 = Card(Rank.Two, Suit.Hearts)
+	print(f"{card1}, {card2}")
+	print(card2 < card1)
+	print(card1 < card2)
+	pass
